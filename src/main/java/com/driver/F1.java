3c949fd44b189f68f6e0088c26c372558fdb52f8
praveench97 <praveench1997@gmail.com>
2023-01-21 17:49:04 +0530

    Trail 91

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -19,7 +19,7 @@ public class F1 extends Car {
          * speed more than 250: gear 6
          */
 
-        if(newSpeed == 0) {
+        if(newSpeed <= 0) {
             //Stop the car, set gear as 1
             changeGear(1);
             stop();
