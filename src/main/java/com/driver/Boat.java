de3a91374b6be3f4955fc648b92f9b38f6ff1dd3
praveench97 <praveench1997@gmail.com>
2023-01-21 17:08:11 +0530

    Trail 3

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -3,6 +3,10 @@ package com.driver;
 public class Boat implements WaterVehicle{
     private String name;
     private int capacity;
+    public void Boat(String name, int capacity){
+        this.name = name;
+        this.capacity = capacity;
+    }
     @Override
     public String getVehicleName() {
         return name;
